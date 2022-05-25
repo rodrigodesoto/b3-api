f0901a1bddb4b59ba6330a05d9ea220941f1309b
Soto <rodrigo.soto@airesdigital.com.br>
2022-05-25 10:39:53 -0300

    Inclusão arquivos yaml

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: false }));
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
