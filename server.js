8efc6fc97497fcbc51357e947e542e1de2c7acd7
Soto <rodrigo.soto@airesdigital.com.br>
2022-11-16 12:08:53 -0300

    Tentativa WS Heroku 6

--- server.js
+++ server.js
@@ -46,81 +46,81 @@ app.use('/quotes', require('./src/quotes/stock.controller'));
 // global error handler
 app.use(errorHandler);
 var portAp = process.env.PORT_AP || 4000;
-var portWs = process.env.PORT_WS || 3000;
+// var portWs = process.env.PORT_WS || 3000;
 
 // Create an HTTP service.
 http.createServer(app).listen(portAp);
 console.log('Server http listening on port ' + portAp);
 
-const server = app.listen(portWs, () => {
-    console.log('WebSocket is running on port ' + portWs);
-})
+// const server = app.listen(portWs, () => {
+//     console.log('WebSocket is running on port ' + portWs);
+// })
 
-const wss = appWs(server);
-
-setInterval(async () => {
-    try {
-        for (let i = 0; i < Object.keys(tickerEnum).length; i++) {
-            await getCurrentQuote(Object.keys(tickerEnum)[i].toString(), await function(err, quote){
-                if(quote){
-                    wss.broadcast({ id: i, ticker: Object.keys(tickerEnum)[i].toString() , quote: quote });
-                    console.log({id: i, ticker: Object.keys(tickerEnum)[i].toString(), quote: quote});
-                }
-            });
-        }
-    } catch (error) {
-        return error;
-    }
-}, 10000);
-
-async function getCurrentQuote(ticker, callback) {
-    const response = await fetch("https://finance.yahoo.com/quote/" + ticker + ".sa/")
-    const body = await response.text();
-    const main = JSON.parse(
-        body.split("root.App.main = ")[1].split(";\n}(this));")[0]
-    );
-    let quote = {};
-
-    if (
-        main.context.dispatcher.stores?.QuoteSummaryStore.financialData !==
-        undefined
-    ) {
-        quote.price = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.financialData
-                .currentPrice.fmt
-        );
-        // quote.price = quote.price + Math.random()
-        quote.open = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketOpen
-                .fmt
-        );
-        quote.high = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.price
-                .regularMarketDayHigh.fmt
-        );
-        quote.low = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.price
-                .regularMarketDayLow.fmt
-        );
-        quote.previousClose = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.price
-                .regularMarketPreviousClose.fmt
-        );
-        quote.volume = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.price
-                .regularMarketVolume.fmt
-        );
-        quote.marketChange = parseFloat(
-            main.context.dispatcher.stores.QuoteSummaryStore.price
-                .regularMarketChangePercent.fmt
-        );
-        quote.shortName =
-            main.context.dispatcher.stores.QuoteSummaryStore.price.shortName;
-        quote.longName =
-            main.context.dispatcher.stores.QuoteSummaryStore.price.longName;
-    }
-
-    callback(null, quote);
-};
+// const wss = appWs(server);
+//
+// setInterval(async () => {
+//     try {
+//         for (let i = 0; i < Object.keys(tickerEnum).length; i++) {
+//             await getCurrentQuote(Object.keys(tickerEnum)[i].toString(), await function(err, quote){
+//                 if(quote){
+//                     wss.broadcast({ id: i, ticker: Object.keys(tickerEnum)[i].toString() , quote: quote });
+//                     console.log({id: i, ticker: Object.keys(tickerEnum)[i].toString(), quote: quote});
+//                 }
+//             });
+//         }
+//     } catch (error) {
+//         return error;
+//     }
+// }, 10000);
+//
+// async function getCurrentQuote(ticker, callback) {
+//     const response = await fetch("https://finance.yahoo.com/quote/" + ticker + ".sa/")
+//     const body = await response.text();
+//     const main = JSON.parse(
+//         body.split("root.App.main = ")[1].split(";\n}(this));")[0]
+//     );
+//     let quote = {};
+//
+//     if (
+//         main.context.dispatcher.stores?.QuoteSummaryStore.financialData !==
+//         undefined
+//     ) {
+//         quote.price = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.financialData
+//                 .currentPrice.fmt
+//         );
+//         // quote.price = quote.price + Math.random()
+//         quote.open = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketOpen
+//                 .fmt
+//         );
+//         quote.high = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.price
+//                 .regularMarketDayHigh.fmt
+//         );
+//         quote.low = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.price
+//                 .regularMarketDayLow.fmt
+//         );
+//         quote.previousClose = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.price
+//                 .regularMarketPreviousClose.fmt
+//         );
+//         quote.volume = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.price
+//                 .regularMarketVolume.fmt
+//         );
+//         quote.marketChange = parseFloat(
+//             main.context.dispatcher.stores.QuoteSummaryStore.price
+//                 .regularMarketChangePercent.fmt
+//         );
+//         quote.shortName =
+//             main.context.dispatcher.stores.QuoteSummaryStore.price.shortName;
+//         quote.longName =
+//             main.context.dispatcher.stores.QuoteSummaryStore.price.longName;
+//     }
+//
+//     callback(null, quote);
+// };
 
 module.exports = app;
